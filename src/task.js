ed335aba5de67d1504f65d4cc5a063d5915ad51d
eternalwill43 <emilsmoney@live.com>
2023-08-05 06:01:45 -0400

    feat: add some validation functions and checks to the functions

--- src/task.js
+++ src/task.js
@@ -13,9 +13,21 @@ export default function createTask(title, description, dueDate, priority, notes,
       this.description = newDes;
     },
     setDate(newDate) {
+      if (!this.isValidDate(newDate)) {
+        throw new Error("Invalid date format. Please use \"mm/dd/yyyy\"");
+      }
       this.dueDate = newDate;
     },
+    /**
+     * Sets priority of the task, can throw an error if the priority is invalid
+     * @param {string} newPrio priority (has to be either low, medium, high or urgent)
+     * @returns {void | Error} either throws an error changes the priority
+    */
     setPrio(newPrio) {
+      let validPriorities = ["low", "medium", "high", "urgent"];
+      if (!validPriorities.includes(newPrio)) {
+        throw new Error("Invalid priority, please use [low, medium, high, urgent]");
+      }
       this.priority = newPrio;
     },
     setNotes(newNotes) {
@@ -23,6 +35,15 @@ export default function createTask(title, description, dueDate, priority, notes,
     },
     check() {
       this.checklist = !this.checklist;
-    }
+    },
+    /**
+     * This function should return a boolean if the date format is correct.
+     * @param {string} date the date representing the string
+     * @returns {boolean} boolean representing if the input string valid or not
+    */
+    isValidDate(date) {
+      // TODO: Implement input validation
+      return false;
+    },
   };
 }
