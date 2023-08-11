482fcb130dd6540164028520e1fcabcd0ad6a9b3
EternalWill43 <emilsmoney@live.com>
2023-08-11 10:01:21 -0400

    feat: create functions that take in complex nested objects and generate HTML for the nested objects

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 // @ts-check
+// Imports for data logic
+import createPortfolio from "./portfolio.js"; // import createToDo from "./todo.js";
+import newProject from "./project.js";
+import createTask from "./task.js";
+import createToDo from "./todo.js";
+// Imports for DOM manipulation
+import { createPortfolioDiv } from "./domcontroller.js"
 
-// import createPortfolio from "./portfolio.js";
-// import createToDo from "./todo.js";
-// import newProject from "./project.js";
-// import createTask from "./task.js";
-// const portfolio = createPortfolio("My Projects");
-// const project = newProject("The Odin Project");
-// const todo = createToDo("My to-do list");
-// const firstTask = createTask(
-//   "Make a new list",
-//   "create a to-do list",
-//   "03/08/2023",
-//   "high",
-//   "make the structure",
-//   false
-// );
-// todo.addTask(firstTask);
-// project.addTodo(todo);
-// portfolio.addProject(project);
-// portfolio.projects[0].todos[0].tasks[0].setDes("Some new description");
-// console.log(portfolio);
+const portfolio = createPortfolio("Portfolio");
+const project = newProject("The Odin Project");
+const todo = createToDo("My to-do list");
+const firstTask = createTask(
+    "Make a new list",
+    "create a to-do list",
+    "03/08/2023",
+    "high",
+    "make the structure",
+    false
+);
+todo.addTask(createTask(
+    "Some random task",
+    "Some random description",
+    "08/12/2023",
+    "high",
+    "Some random notes",
+    false
+));
+todo.addTask(firstTask);
+project.addTodo(todo);
+portfolio.addProject(project);
+portfolio.projects[0].todos[0].tasks[0].setDes("Some new description");
+console.log(portfolio);
 
-import addProjectDiv from "./domcontroller.js";
-
-const firstProject = addProjectDiv("First Project");
-console.log(firstProject);
+document.body.appendChild(createPortfolioDiv(portfolio));
